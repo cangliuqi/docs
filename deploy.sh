65e8264c58e984937cc4f39193036fc706dca864
YangYitao <1365065894@qq.com>
2020-05-14 16:01:26 +0800

    [update]测试

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# git push -f git@github.com:7revor/7revor.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
@@ -26,3 +26,32 @@ git commit -m 'deploy'
 git push -f https://${access_token}@github.com/cangliuqi/docs.git master:gh-pages
 
 cd -
+
+##!/usr/bin/env sh
+#
+## 确保脚本抛出遇到的错误
+#set -e
+#
+## 生成静态文件
+#npm run docs:build
+#
+## 进入生成的文件夹
+#cd docs/.vuepress/dist
+#
+## 如果是发布到自定义域名
+## echo 'www.example.com' > CNAME
+#
+#git init
+#git add -A
+#git commit -m 'deploy'
+#
+## 如果发布到 https://<USERNAME>.github.io
+## git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+#
+## 如果发布到 https://<USERNAME>.github.io/<REPO>
+## git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+#
+## 如果使用 travis 持续集成
+#git push -f https://${access_token}@github.com/cangliuqi/docs.git master:gh-pages
+#
+#cd -
